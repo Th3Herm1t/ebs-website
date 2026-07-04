5b72298767141fca7df108527721b9e574aa34cb
Abdelkarim Ben Salah <abdelkarim.bensaleh@istls.u-sousse.tn>
2026-07-04 22:17:04 +0200

    feat: implement HeroSection component with animated course category carousel

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -132,7 +132,7 @@ export default function HeroSection() {
               className="relative inline-block w-full max-w-[500px] lg:max-w-[700px]"
             >
               <Image
-                src="/images/all-img/hero-girl-new-copy.png"
+                src="/images/all-img/hero-batch7.png"
                 alt="EBS Student"
                 width={800}
                 height={900}
